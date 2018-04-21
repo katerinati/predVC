b822975091bed8a8fac5e3d0a5f34540b503cec3
Kate <katerina_ti@mail.ru>
2018-04-21 19:22:27 +0100

    stable version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,52 +4,55 @@ $('#myTabs a').click(function (e) {
   $(this).tab('show')
 });
 
+function createChart (selector) {
+  var ctx = document.querySelector(selector).getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: 'line',
-
-  // The data for our dataset
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-      label: "History",
-      backgroundColor: 'rgba(0, 255, 0, 0.01)',
-      borderColor: '#21cf98',
-      data: [0, 80, 5, 70, 20, 30, 45],
-    }]
-  },
-
-  // Configuration options go here
-  options: {
-    legend: {
-      display: false
-    },
-    title: {
-      display: false,
-      text: "History",
-      position: "top"
-    },
-    scales: {
-      yAxes: [{
-        ticks: {
-          display: false
-        },
-        gridLines: {
-          color: 'rgba(0, 0, 0, 0.05)',
-          // drawBorder: false
-        }
-      }],
-      xAxes: [{
-        ticks: {
-          display: false
-        },
-        gridLines: {
-          color: 'rgba(0, 0, 0, 0.05)',
-          // drawBorder: false
-        }
+    // The data for our dataset
+    data: {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [{
+        label: "History",
+        backgroundColor: 'rgba(0, 255, 0, 0.01)',
+        borderColor: '#21cf98',
+        data: [0, 80, 5, 70, 20, 30, 45],
       }]
+    },
+
+    // Configuration options go here
+    options: {
+      legend: {
+        display: false
+      },
+      title: {
+        display: false,
+        text: "History",
+        position: "top"
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: 'rgba(0, 0, 0, 0.05)',
+            // drawBorder: false
+          }
+        }],
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: 'rgba(0, 0, 0, 0.05)',
+            // drawBorder: false
+          }
+        }]
+      }
     }
-  }
-});
+  });
+}
+
+createChart('.chartTeam canvas');
