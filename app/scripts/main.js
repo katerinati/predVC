daf9700ead1f56be827858bf223bcf4a24b75eb3
Kate <katerina_ti@mail.ru>
2018-04-21 18:52:11 +0100

    stable version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,53 @@ $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+      label: "History",
+      backgroundColor: 'rgba(0, 255, 0, 0.01)',
+      borderColor: '#21cf98',
+      data: [0, 80, 5, 70, 20, 30, 45],
+    }]
+  },
+
+  // Configuration options go here
+  options: {
+    legend: {
+      display: false
+    },
+    title: {
+      display: false,
+      text: "History",
+      position: "top"
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          color: 'rgba(0, 0, 0, 0.05)',
+          // drawBorder: false
+        }
+      }],
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          color: 'rgba(0, 0, 0, 0.05)',
+          // drawBorder: false
+        }
+      }]
+    }
+  }
+});
